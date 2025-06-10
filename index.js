9512761d1165d77592581372fa647101ae6fd94c
wellpence5 <alvinmuthomi22@gmai.com>
2025-06-10 11:48:10 +0300

    finally

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if(rideDistance <= 400){
+    return "This one is on me!"
+  }else if(rideDistance < 2000){
+    return "That will be twenty bucks."
+  }else if(rideDistance <= 2500){
+    return "I will gladly take your thirty bucks."
+  }else
+    {return "No can do."}
+};
+
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous"){
+    return "Thank you so much."
+  }else if(tip === "not as generous"){
+    return "Thank you."
+  }else {
+    return "Bye."
+  }
 }
